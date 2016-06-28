3d1390cb8fd9934c78a9d8ca52ebe143a4ade477
Raymond Chou <Raymond Chou@sickkids.ca>
2016-06-28 13:25:30 -0400

    Test Program

--- GOtoSQL_test.go
+++ GOtoSQL_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"database/sql"
+	"reflect"
+	"testing"
+)
 
 func TestMatchTable(t *testing.T) {
 	tablenames := []string{"contact info", "contactInfo", "contactin19fo", "infoinfo", "inf"}
@@ -16,8 +20,40 @@ func TestMatchTable(t *testing.T) {
 		}
 	}
 	if nummatched != len(expectedPHItablenames) {
-		t.Errorf("TestmatchTable failed because %d matched instead of %d", nummatched, len(expectedPHItablenames))
+		t.Errorf("TestMatchTable failed because %d matched instead of %d", nummatched, len(expectedPHItablenames))
 	} else {
 		t.Log("MatchTable Works")
 	}
+}
+func TestConvertToString(t *testing.T) {
+	vals := make([]interface{}, 10)
+	for i := 0; i < 10; i++ {
+		vals[i] = new(sql.NullString)
+	}
+	stringval := make([]string, 10)
+	row := convertToString(vals)
+	actualtype := reflect.TypeOf(row)
+	expectedtype := reflect.TypeOf(stringval)
+
+	if actualtype != expectedtype {
+		t.Errorf("TestConvertToString failed because actual type is %s whereas expected type is %s", actualtype, expectedtype)
+	} else {
+		t.Log("convertToString Works")
+	}
+}
+func TestConvertToText(t *testing.T) {
+	rowstring := make([]string, 10)
+	for i := range rowstring {
+		rowstring[i] = "Hello1&3"
+	}
+	actualrow := convertToText(rowstring)
+	expectedrow := "\nHello1&3|Hello1&3|Hello1&3|Hello1&3|Hello1&3|Hello1&3|Hello1&3|Hello1&3|Hello1&3|Hello1&3"
+	if actualrow == expectedrow {
+		t.Error("convertToText Works")
+	} else {
+		t.Errorf("TestConvertToText failed because the converted string is %s instead of %s", actualrow, expectedrow)
+	}
+}
+func TestFileWrite(t *testing.T) {
+
 }
