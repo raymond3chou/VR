5ab9230fbb7c6cb72d2a031817f85588435dbba0
Raymond Chou <Raymond Chou@sickkids.ca>
2016-07-14 14:15:02 -0400

    EchoAndTED Complete

--- EchoAndTED/EchoAndTED.go
+++ EchoAndTED/EchoAndTED.go
@@ -6,22 +6,22 @@ import (
 	"log"
 	"strings"
 
+	"github.com/access"
 	"github.com/tealeg/xlsx"
 )
 
 //EchoPaths keeps track of all the Echo file paths
-var EchoPaths []string
+var echoPaths []string
+var tedPaths []string
 
-//Go Through All Databases
+//walkDirectory goes through the current folder and looks for excel files and folders
 func walkDirectory(folderNames []string, dir string) {
 	for _, folderName := range folderNames {
+		xlsxNames := []string{}
 		newDir := dir + folderName + "\\"
 		xlsxNames, newfolderNames := findExcel(newDir)
-		for _, xlsx := range xlsxNames {
-			xlsxPath := newDir + xlsx
-			if checkExcel(xlsxPath, "Echo") {
-				EchoPaths = append(EchoPaths, newDir+xlsx)
-			}
+		if len(xlsxNames) != 0 {
+			checkExcel(xlsxNames, newDir)
 		}
 		walkDirectory(newfolderNames, newDir)
 	}
@@ -48,16 +48,70 @@ func findExcel(dir string) ([]string, []string) {
 	return xlsxNames, folderNames
 }
 
-func checkExcel(xlsxPath string, checkCriteria string) bool {
+//checkSheet checks if the excel file is an echo file by checking if echo is present in the header columns
+func checkSheet(xlsxPath string, checkCriteria string, path string) bool {
+	log.Println(xlsxPath)
 	sheet, err := xlsx.OpenFile(xlsxPath)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatalln(err)
 	}
-	for _, v := range sheet.Sheets[0].Rows[0].Cells {
-		fmt.Println(v.Value)
-		if v.Value == checkCriteria {
-			return true
+	if !checkEmptyHeader(sheet) {
+
+		for i := 0; i < sheet.Sheets[0].MaxCol; i++ {
+			cellValue := sheet.Sheets[0].Cell(0, i).Value
+			cellValue = strings.ToLower(cellValue)
+			if strings.Contains(cellValue, checkCriteria) {
+				xlsxPathNew := strings.TrimPrefix(xlsxPath, "L:\\CVDMC Students\\valve_registry\\")
+				newName := strings.Replace(xlsxPathNew, "\\", "_", -1)
+				fmt.Println(path + newName)
+				sheet.Save(path + newName)
+				return true
+			}
 		}
 	}
 	return false
+}
+
+//checkTED checks if the excel file is a ted file by checking if there is ted in the name
+func checkTED(xlsx string, checkCriteria string) bool {
+	xlsx = strings.ToLower(xlsx)
+	if strings.Contains(xlsx, checkCriteria) {
+		return true
+	}
+	return false
+}
+
+//checkExcel goes through the excel files and checks if they're echo files or ted files
+func checkExcel(xlsxNames []string, newDir string) {
+	for _, xlsx := range xlsxNames {
+		xlsxPath := newDir + xlsx
+		log.Printf("Checking file: %s", xlsx)
+		if checkSheet(xlsxPath, "graftsz", "L:\\CVDMC Students\\Raymond Chou\\ted\\") {
+			tedFile, _ := access.ConnectToTxt("C:\\Users\\raymond chou\\Desktop\\tedFiles.txt")
+			access.FileWrite(tedFile, xlsxPath+"\n")
+			tedFile.Close()
+		}
+		if checkSheet(xlsxPath, "echo", "L:\\CVDMC Students\\Raymond Chou\\echo\\") {
+			echoFile, _ := access.ConnectToTxt("C:\\Users\\raymond chou\\Desktop\\echoFiles.txt")
+			access.FileWrite(echoFile, xlsxPath+"\n")
+			echoFile.Close()
+		}
+	}
+}
+
+//checkEmptyHeader checks if the header is empty
+func checkEmptyHeader(sheet *xlsx.File) bool {
+	if sheet.Sheets[0].Cell(0, 0).Value == "" {
+		return true
+	}
+	return false
+}
+
+func main() {
+	dir := "L:\\CVDMC Students\\valve_registry\\"
+	folderNames := []string{""}
+	errFile := access.CreateErrorLog(true)
+	log.SetOutput(errFile)
+	defer errFile.Close()
+	walkDirectory(folderNames, dir)
 }
--- EchoAndTED/EchoAndTED_test.go
+++ EchoAndTED/EchoAndTED_test.go
@@ -1,15 +1,29 @@
 package main
 
-import "testing"
+import (
+	"log"
+	"testing"
+
+	"github.com/access"
+)
 
 func TestWalkDirectory(t *testing.T) {
+	errFile := access.CreateErrorLog(true)
+	log.SetOutput(errFile)
+	defer errFile.Close()
 	dir := "C:\\Users\\raymond chou\\Desktop\\WorkingFiles\\VR\\EchoAndTED\\"
 	folderNames := []string{"Example"}
+
 	walkDirectory(folderNames, dir)
-	// if EchoPaths[0] != "C:\\Users\\raymond chou\\Desktop\\WorkingFiles\\VR\\EchoAndTED\\Example\\Test.xlsx" {
-	// 	t.Errorf("The Path was %s and not %s", EchoPaths[0], "C:\\Users\\raymond chou\\Desktop\\WorkingFiles\\VR\\EchoAndTED\\Example\\Test.xlsx")
-	// }
-	if EchoPaths[0] != "C:\\Users\\raymond chou\\Desktop\\WorkingFiles\\VR\\EchoAndTED\\Example\\Test Folder\\Echo.xlsx" {
-		t.Errorf("The Path was %s and not %s", EchoPaths[0], "C:\\Users\\raymond chou\\Desktop\\WorkingFiles\\VR\\EchoAndTED\\Example\\Test Folder\\Echo.xlsx")
+
+	tedFile := access.ReadFile("C:\\Users\\raymond chou\\Desktop\\tedFiles.txt")
+	if tedFile != "C:\\Users\\raymond chou\\Desktop\\WorkingFiles\\VR\\EchoAndTED\\Example\\Test Folder\\tedcodes.xlsx\n" {
+		t.Errorf("Printed %s instead of C:\\Users\\raymond chou\\Desktop\\WorkingFiles\\VR\\EchoAndTED\\Example\\Test Folder\\tedcodes.xlsx\n", tedFile)
 	}
+
+	echoFile := access.ReadFile("C:\\Users\\raymond chou\\Desktop\\echoFiles.txt")
+	if echoFile != "C:\\Users\\raymond chou\\Desktop\\WorkingFiles\\VR\\EchoAndTED\\Example\\Test Folder\\Echo.xlsx\nC:\\Users\\raymond chou\\Desktop\\WorkingFiles\\VR\\EchoAndTED\\Example\\Test Folder\\Echo2.xlsx\n" {
+		t.Errorf("Printed %s instead of C:\\Users\\raymond chou\\Desktop\\WorkingFiles\\VR\\EchoAndTED\\Example\\Test Folder\\Echo.xlsx\nC:\\Users\\raymond chou\\Desktop\\WorkingFiles\\VR\\EchoAndTED\\Example\\Test Folder\\Echo2.xlsx\n", echoFile)
+	}
+
 }
