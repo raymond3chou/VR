c97fb9bacb575ed0bed747d2e2d9d0603e696675
Raymond Chou <Raymond Chou@sickkids.ca>
2016-08-02 16:30:10 -0400

    Start PHI JSON Convertor

---
+++ PHIJsonConvertor/JsonConvertor.go
@@ -0,0 +1,16 @@
+package main
+
+//query for PTID
+//collections : email, phone, address, personal info, cardio, GP
+//email:
+//for each unique email take date, ptid
+//phone:
+//for each unique phone take date,PTID
+//address:
+//for each unique address take date,PTID
+//cardio:
+//for each unique cardio take date,PTID
+//GP:
+//for each unique GP take date,PTID
+//personal info:
+//should be only one set, match with VR
